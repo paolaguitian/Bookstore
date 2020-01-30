0bf168a388a2013bcd79fc33c8f69a2995d668c5
ahabbaba1 <aysha.habbaba@gmail.com>
2020-01-30 13:22:06 -0500

    created review model

---
+++ models/reviews.js
@@ -0,0 +1,28 @@
+import sequelize, { DataTypes, Sequelize } from 'sequelize';
+
+const reviews = sequelize.define('review', {
+    //attributes
+    bookID: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+    },
+    userID: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+    },
+    timeStamp: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    comment: {
+        type: DataTypes.TEXT,
+    },
+    rating: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+    },
+})
