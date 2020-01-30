39599249c6f087958e0e7b277192ed6e6a28e571
AquilaEternus <hernandez.j.98a@gmail.com>
2020-01-30 13:28:17 -0500

    Created book model

---
+++ models/books.js
@@ -0,0 +1,15 @@
+import sequelize, { DataTypes, Sequelize } from 'sequelize';
+
+const books = sequelize.define('books', {
+    bookID: {
+        primaryKey : true,
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    genre : DataTypes.STRING(20),
+    sales : DataTypes.INTEGER,
+    title : DataTypes.STRING(255),
+    releaseDate : DataTypes.STRING(25), 
+    bookCover : DataTypes.STRING(255),
+    publisher : DataTypes.STRING(50),
+})
