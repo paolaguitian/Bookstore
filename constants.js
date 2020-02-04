e5b04cca66c2152d1c641ab4d82679a3d0d68012
AquilaEternus <hernandez.j.98a@gmail.com>
2020-02-04 13:27:19 -0500

    Setting up db variables, models and relationships

---
+++ constants.js
@@ -0,0 +1,12 @@
+const DATABASE_NAME='demo';
+const USERNAME='root';
+const PASSWORD='';
+const HOST='localhost';
+const  DIALECT='mysql';
+module.exports = {
+  DATABASE_NAME: DATABASE_NAME,
+  USERNAME:USERNAME,
+  PASSWORD:PASSWORD,
+  HOST:HOST,
+  DIALECT:DIALECT
+}
---
+++ sequelize.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+
+const AuthorModel = require('./models/authors')
+const BookModel = require('./models/books')
+const WishlistModel = require('./models/wishlists')
+
+const OrderModel = require('./models/orders')
+const ReviewModel = require('./models/reviews')
+
+const UserModel = require('./models/users')
+const ShippingAddressModel =  require('./models/shippingAddress')
+const CreditCardModel = require('./models/creditCards')
+
+
+const {DATABASE_NAME,ROOT,PASSWORD,HOST,DIALECT} =require('./constants')
+
+const sequelize = new Sequelize(DATABASE_NAME, ROOT, PASSWORD,
+  {host: HOST,dialect: DIALECT,pool: {max: 10,min: 0,acquire: 30000,idle: 10000}})
+
+
+//Models
+const Book = BookModel(sequelize, Sequelize)
+const Author = AuthorModel(sequelize, Sequelize)
+const Wishlist = WishlistModel(sequelize, Sequelize)
+const Order = OrderModel(sequelize, Sequelize)
+const Review = ReviewModel(sequelize, Sequelize)
+const User = UserModel(sequelize, Sequelize)
+const ShippingAddress = ShippingAddressModel(sequelize, Sequelize)
+const CreditCard = CreditCardModel(sequelize, Sequelize)
+
+//Associations
