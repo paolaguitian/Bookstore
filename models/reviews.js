f6ff9470e4cecd087c5b75683b5f09d49a3790fc
ahabbaba1 <aysha.habbaba@gmail.com>
2020-02-04 12:23:34 -0500

    removed foreign keys as attributes

--- models/reviews.js
+++ models/reviews.js
@@ -1,18 +1,6 @@
 import sequelize, { DataTypes, Sequelize } from 'sequelize';
 
 const reviews = sequelize.define('review', {
-    //attributes
-    bookID: {
-        primaryKey: true,
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    userID: {
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-    },
     timeStamp: {
         type: DataTypes.DATE,
         allowNull: false,
