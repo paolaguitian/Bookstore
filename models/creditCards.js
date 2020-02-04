bf1560f0b8b12a90c4981f02bd2fc38fa22fdf41
ahabbaba1 <aysha.habbaba@gmail.com>
2020-02-04 12:25:25 -0500

    removed foreign keys as attributes and updated cardNumber type

--- models/creditCards.js
+++ models/creditCards.js
@@ -1,15 +1,9 @@
 import sequelize, { DataTypes, Sequelize } from 'sequelize';
 
 const creditCards = sequelize.define('creditCard', {
-    userID: {
-        primaryKey: true,
-        type: DataTypes.UUID,
-        default: sequelize.UUIDV4,
-        allowNull = false,
-    },
     //maximum card size is 19 digits
     cardNumber: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
     },
     //maximum is 3 digits
