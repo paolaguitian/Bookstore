90de8e5317c6a43ba40ab9bcaf5ebb103dcea3ae
Paola Guitian <poliguitian@gmail.com>
2020-02-21 17:05:42 -0500

    set up modal flow

--- client/src/views/login/login.js
+++ client/src/views/login/login.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false,
+    }
+  }
+
+  openModal = () => {
+    this.setState({
+      showModal: true,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+    })
+  }
+
+  submitForm = () => {
+    console.log("write the logic to submit form to create user or log them in")
+  }
+
   render() {
     return (
       <div>
-        <Button type="dashed" icon="user" size="default">
+        <Button
+          type="dashed"
+          icon="user"
+          onClick={this.openModal}>
           Hello, Sign In
         </Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.showModal}
+          onOk={this.submitForm}
+          onCancel={this.closeModal}
+        >
+          <p>Login Fields...</p>
+        </Modal>
       </div>
     )
   }
