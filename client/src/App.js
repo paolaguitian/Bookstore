6f3e72363434df89bc0fdd71f4c7d15c95ffae31
Paola Guitian <poliguitian@gmail.com>
2020-03-13 22:53:28 -0400

    Co-authored-by: Alejandro Corredor <acorredor@stoalabs.com>

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,35 @@ import Home from './views/home/home';
 import BookDetails from './components/bookdetails'
 import AuthorCatalog from './components/authordetails/authorcatalog'
 import './App.css'
+import UserContext from './context';
 
 class App extends Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {}
+    };
+  }
+
+  render() {
     return (
       <Router>
+        <UserContext.Provider value={{
+          state: this.state,
+          setState: this.setState.bind(this),
+        }}>
         <div>
           <Link to="/"/>
            <Switch>
-             
              <Route path="/" exact component={Home} />
              <Route path="/bookdetails/:bookID" exact component={BookDetails} />
              <Route path="/authorlisting/:authorID" component={AuthorCatalog} />
              <Route path="/?page=:pageNum/?isbn=:bookID" component={BookDetails} />
+             {/* create private routing and authentication for dashboard */}
+             <Route path="/dashboard" component={() => <div>Welcome to `${this.state.user.userID}`'s dashboard!</div>} />
            </Switch>
         </div>
+        </UserContext.Provider>
       </Router>
     );
   }
